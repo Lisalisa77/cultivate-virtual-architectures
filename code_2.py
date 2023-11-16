5d23fb8422e9f50d1349f7f58fc4cdcc2322e822
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-11-17 00:49:00 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,65 @@
+
+        console.log("Random Number: " + item);
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
+}
+    return data;
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+
+function main() {
+function generateRandomData() {
+}
+    });
+    data.forEach(item => {
+    const data = [];}
+    }
+}
+function main() {
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    return data;
+    for (let i = 0; i < 10; i++) {
+
+    data.forEach(item => {
+
+
+    });
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+main();function generateRandomData() {
+
+main();
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+}
+    });
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    return data;
+    const data = [];
+
+    }
+    const data = generateRandomData();
+}
+function main() {}
+    const data = generateRandomData();
+}
+    });
+    for (let i = 0; i < 10; i++) {
+
+
+    return data;
+function main() {
+        console.log("Random Number: " + item);
+main();
+    data.forEach(item => {
+
+    }
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
