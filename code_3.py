3782a069e27b44aed403824612ac061d678d242d
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-11-25 10:30:03 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,64 @@
-    for item in data:
-import random
-    main()
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-    data = generate_random_data()
 
+        }
+        for (int item : data) {
+        int[] data = new int[10];
+    }
+}
+
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }    for item in data:
+if __name__ == "__main__":
+
+
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
     return data
 
+def generate_random_data():
+    data = generate_random_data()
+    main()
 
+def main():
+    main()
+    for item in data:
+def generate_random_data():
+
+import random
+def main():
+if __name__ == "__main__":
+    data = generate_random_data()
 
         print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    for item in data:
+        print(f"Random Number: {item}")
+
+
+def main():
+
+import random
+    main()
 if __name__ == "__main__":
+    return data
+
+if __name__ == "__main__":
+
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
+    data = generate_random_data()
+import random
+    main()
+        print(f"Random Number: {item}")
+def main():
