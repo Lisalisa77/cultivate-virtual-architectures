156e7983ebba1455d6834108a918bde5e7777c1f
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-12-01 01:03:44 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,39 +1,17 @@
-    return data
-        print(f"Random Number: {item}")
+main();
+    return data;
+    }
+    });
+        console.log("Random Number: " + item);
+}
+    data.forEach(item => {
 
-    data = [random.randint(1, 100) for _ in range(10)]
-
-if __name__ == "__main__":
-
-def generate_random_data():
-def main():
-    main()
-
-    for item in data:
-    data = generate_random_data()
-import randomdef main():
-
-def generate_random_data():
-    for item in data:
-        print(f"Random Number: {item}")
-    return data
+    const data = generateRandomData();
+    const data = [];
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
 
 
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-if __name__ == "__main__":
-import random
-
-    data = generate_random_data()
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-if __name__ == "__main__":
-    for item in data:
-def main():
-def generate_random_data():
-
-        print(f"Random Number: {item}")
-import random
-
-    main()
+function main() {
+    for (let i = 0; i < 10; i++) {
+}
